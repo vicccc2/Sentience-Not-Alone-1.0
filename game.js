7915ffc1932c159bcd20eaa8c63388586a27a600
vicccc2 <167596999+vicccc2@users.noreply.github.com>
2024-06-03 17:37:47 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,3 @@
-/*  Students: Please use this week's project for Week 13: Assignment 16: Prototype. 
-     You will need to replace the contents of this JavaScript file with your own work, 
-     and create any other files, if any, required for the assignment.
-     When you are done, be certain to submit the assignment in Canvas to be graded. */
 "use strict";
 
 let game;
